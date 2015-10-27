c42145e71ca03a921db6791bea57710f0da6d0be
dmmanns <dmmanns555@gmail.com>
2015-10-27 14:32:30 -0400

    Exercise3 - What's your name?

---
+++ exercise2.rb
@@ -0,0 +1,6 @@
+tip = 55 * 0.20
+puts "A good tip on a $55 meal is #{tip}"
+puts "Dan is" + " 32"
+result = 45628 * 7839
+puts "The result of 45628 multipled by 7839 is #{result}"
+puts (true && false) || (false && true) || !(false && false)
